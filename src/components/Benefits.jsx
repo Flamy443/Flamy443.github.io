a909ce8c2c6a349c2d9476e5cef76f37e15925b1
atharao <raoathar.200@gmail.com>
2024-09-07 22:50:03 +0530

    initial commit

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,7 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
-import Arrow from "../assets/svg/Arrow";
+// import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
